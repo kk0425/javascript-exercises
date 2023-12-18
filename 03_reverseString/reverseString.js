8ac401e418ac839719b9d7d0cf0044d5be6a34f0
kk0425 <kk0425@gmail.com>
2023-12-18 00:52:22 -0500

    exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const splitStr = str.split('');
+    let reversedStr = '';
+    for (let i = splitStr.length - 1; i >= 0; i--) {
+        reversedStr += splitStr[i];
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
