40ab823f0d39dde4575f0df2f51b6fd2135af317
kk0425 <kk0425@gmail.com>
2023-12-18 00:33:00 -0500

    exercise 2 solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    let strOutput = '';
+    for (let i = 0; i < num; i++) {
+        strOutput += string;
+    }
+    return strOutput;
 };
 
 // Do not edit below this line
