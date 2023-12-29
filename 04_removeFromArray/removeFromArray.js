80d5dc7e1d44a8c790cc332f1a81aa7775ff5f1a
kk0425 <kk0425@gmail.com>
2023-12-29 02:13:09 -0500

    lesson 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,30 @@
-const removeFromArray = function() {
+/*
+function removeFromArray(array, ...values) {
+    return array.filter(arrayValue => {
+        for (const value of values) {
+            if (value === arrayValue) {
+                return false;
+            }
+        }
+        return true;
+    })
+}
+*/
 
-};
+function removeFromArray(numbers, ...removeValues) {
+    const result = [];
+    for (const number of numbers) {
+        let numberInList = false;
+        for (const removeValue of removeValues) {
+            if (removeValue === number) {
+                numberInList = true;
+                break;
+            }
+        }
+        if (!numberInList) result.push(number);
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
