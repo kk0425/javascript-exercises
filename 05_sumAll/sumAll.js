7293256a97068de286c8b3556ec3d2b7f1c4a9ab
kk0425 <kk0425@gmail.com>
2023-12-31 01:20:04 -0500

    challenge 5 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    if (firstNum < 0 || lastNum < 0 || typeof firstNum !== "number" || typeof lastNum !== "number") {
+        return "ERROR";
+    }
+    let sum = 0;
+    const minNum = Math.min(firstNum, lastNum);
+    const maxNum = Math.max(firstNum, lastNum);
+    for (let i = minNum; i <= maxNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
