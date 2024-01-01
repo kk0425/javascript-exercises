db43a2c5b7d3d1c80049130ca5e74eea7c64468b
kk0425 <kk0425@gmail.com>
2024-01-01 06:10:14 -0500

    lesson 8 solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((acc, value) => acc + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((acc, value) => acc * value, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for (let i = num; i >= 2; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
