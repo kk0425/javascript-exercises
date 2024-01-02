0d3ec621acd8f1cca28858338f29bd0a5d1299bd
kk0425 <kk0425@gmail.com>
2024-01-02 04:52:13 -0500

    lesson 8 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    let a = 1;
+    let b = 1;
+    for (let i = 1; i < num; i++) {
+        const c = a + b;
+        a = b;
+        b = c;
+    }
+    return a;
 };
 
 // Do not edit below this line
