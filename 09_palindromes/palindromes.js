34984e849a062106acd6e3e5e293b9918b2802ea
kk0425 <kk0425@gmail.com>
2024-01-02 02:56:49 -0500

    lesson 9 solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const palindrome = str.toLowerCase().match(/[a-z0-9]/g);
+    for (let i = 0; i < palindrome.length / 2; i++) {
+        if (palindrome[i] !== palindrome[palindrome.length - (i + 1)]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
